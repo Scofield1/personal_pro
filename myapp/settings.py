aa720da16b2a37011c11f16f2d737748f18686a3
Scofield1 <adebayoabdulrauf1@gmail.com>
2022-11-09 20:32:45 +0100

    last commit

--- myapp/settings.py
+++ myapp/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'AS6MQYiutKIyHDDOzZbo',
+        'HOST': 'containers-us-west-60.railway.app',
+        'PORT': '6851',
     }
 }
 
